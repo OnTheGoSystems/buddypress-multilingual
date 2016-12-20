69bcc6f174115554e33a2dc20790abb805951475
srdjan <srdjan@icanlocalize.com>
2016-12-20 10:01:55 +0100

    comp-2241 fatal error fix for PHP 5.4

--- includes/class.filters.php
+++ includes/class.filters.php
@@ -170,7 +170,9 @@ class BPML_Filters
     }
 
     public function wpml_fix_activity_redirection( $q ){
-	    if ( empty( $q->get( 'page_id' ) ) && !empty( $q->get( 'pagename' ) ) ) {
+	    if ( !bp_is_blog_page()
+	        && (bool) $q->get( 'page_id' ) === false
+	        && (bool) $q->get( 'pagename' ) === true ) {
 		    if ( is_null( $this->bp_current_page_id ) ) {
 			    $bp_pages = bp_core_get_directory_pages();
 			    $bp_current_component = bp_current_component();
