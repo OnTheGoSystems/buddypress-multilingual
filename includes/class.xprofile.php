e5b0a42d6ff1d9421bf0780f0c63da572c8fdfc7
David Garcia Watkins <david.gw@onthegosystems.com>
2021-11-19 12:18:44 +0100

    [FIXUP] Refactoring

--- includes/class.xprofile.php
+++ includes/class.xprofile.php
@@ -28,17 +28,17 @@ class BPML_XProfile implements \IWPML_Backend_Action, \IWPML_Frontend_Action {
         add_action( 'delete_xprofile_field_meta', array( $this, 'delete_alternate_name' ), 10, 3 );
 
         // Translation filters
-        add_filter( 'bp_get_the_profile_field_name', array($this, 't_name'), self::PRIORITY_BEFORE_NAME_REPLACE );
-        add_filter( 'bp_get_the_profile_field_alternate_name', array($this, 't_alternate_name') );
-        add_filter( 'bp_get_the_profile_field_description', array($this, 't_description') );
-        add_filter( 'bp_xprofile_field_get_children', array($this, 't_options') );
-        add_filter( 'bp_get_the_profile_field_options_checkbox', array($this, 't_checkbox'), 0, 5 );
-        add_filter( 'bp_get_the_profile_field_options_radio', array($this, 't_radio'), 0, 5 );
-        add_filter( 'bp_get_the_profile_field_options_multiselect', array($this, 't_multiselect_option'), 0, 5 );
-        add_filter( 'bp_get_the_profile_field_options_select', array($this, 't_select_option'), 0, 5 );
-        add_filter( 'bp_get_the_profile_field_value', array($this, 't_value_profile_view'), 9, 2 );
-        add_filter( 'bp_get_the_profile_group_name', array($this, 't_group_name') );
-        add_filter( 'bp_get_member_profile_data', array($this, 't_data'), 10, 2 );
+        add_filter( 'bp_get_the_profile_field_name', array($this, 'translate_name'), self::PRIORITY_BEFORE_NAME_REPLACE );
+        add_filter( 'bp_get_the_profile_field_alternate_name', array($this, 'translate_alternate_name') );
+        add_filter( 'bp_get_the_profile_field_description', array($this, 'translate_description') );
+        add_filter( 'bp_xprofile_field_get_children', array($this, 'translate_options') );
+        add_filter( 'bp_get_the_profile_field_options_checkbox', array($this, 'translate_checkbox'), 0, 5 );
+        add_filter( 'bp_get_the_profile_field_options_radio', array($this, 'translate_radio'), 0, 5 );
+        add_filter( 'bp_get_the_profile_field_options_multiselect', array($this, 'translate_multiselect_option'), 0, 5 );
+        add_filter( 'bp_get_the_profile_field_options_select', array($this, 'translate_select_option'), 0, 5 );
+        add_filter( 'bp_get_the_profile_field_value', array($this, 'translate_value_profile_view'), 9, 2 );
+        add_filter( 'bp_get_the_profile_group_name', array($this, 'translate_group_name') );
+        add_filter( 'bp_get_member_profile_data', array($this, 'translate_data'), 10, 2 );
     }
 
     public function bp_init() {
@@ -204,75 +204,128 @@ class BPML_XProfile implements \IWPML_Backend_Action, \IWPML_Frontend_Action {
 	    }
     }
 
-    public function t_name( $name ) {
-        global $field;
-        return stripslashes( apply_filters( 'wpml_translate_single_string', $name, $this->_context, "{$this->_field_string_prefix}{$field->id} name" ) );
+    /**
+     * @param string $value
+     * @param string $name
+     *
+     * @return string
+     */
+    private function translate( $value, $name ) {
+        return stripslashes( apply_filters( 'wpml_translate_single_string', $value, $this->_context, $name ) );
     }
 
-    public function t_alternate_name( $name ) {
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    public function translate_name( $name ) {
         global $field;
-        return stripslashes( apply_filters( 'wpml_translate_single_string', $name, $this->_context, "{$this->_field_string_prefix}{$field->id} alternate name" ) );
+
+        return $this->translate( $name, $this->_field_string_prefix . $field->id . ' name' );
     }
 
-    public function t_description( $description ) {
+    /**
+     * @param string $name
+     *
+     * @return string
+     */
+    public function translate_alternate_name( $name ) {
         global $field;
-        return stripslashes( apply_filters( 'wpml_translate_single_string', $description, $this->_context, "{$this->_field_string_prefix}{$field->id} description" ) );
+
+        return $this->translate( $name, $this->_field_string_prefix . $field->id . ' alternate name' );
     }
 
-    public function t_options( $options ) {
+    /**
+     * @param string $description
+     *
+     * @return string
+     */
+    public function translate_description( $description ) {
         global $field;
+
+        return $this->translate( $description, $this->_field_string_prefix . $field->id . ' description' );
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return array
+     */
+    public function translate_options( $options ) {
+        global $field;
+
         foreach ( $options as &$option ) {
             // Just translate description. Name can messup forms.
             if ( !empty( $option->description ) ) {
-                $option->description = apply_filters( 'wpml_translate_single_string', $option->description,
-	                $this->_context, $this->sanitize_option_basename( $option, $field->id ) . ' description' );
+                $option->description = $this->translate( $option->description, $this->sanitize_option_basename( $option, $field->id ) . ' description' );
             }
         }
+
         return $options;
     }
 
-    protected function _t_option_name( $option, $field_id ) {
-        if ( !empty( $option->name ) ) {
-            return stripslashes( apply_filters( 'wpml_translate_single_string', $option->name, $this->_context,
-                    $this->sanitize_option_basename( $option, $field_id ) . ' name' ) );
+    /**
+     * @param object $option
+     * @param int    $field_id
+     *
+     * @return string
+     */
+    private function translate_option_name( $option, $field_id ) {
+        if ( ! empty( $option->name ) ) {
+            return $this->translate( $option->name, $this->sanitize_option_basename( $option, $field_id ) . ' name' );
         }
+
         return isset( $option->name ) ? $option->name : '';
+    }
+
+    /**
+     * @param object $option
+     * @param int    $field_id
+     *
+     * @return string
+     */
+    private function sanitize_option_basename( $option, $field_id ) {
+        $sanitized_string = bpml_sanitize_string_name( $option->name, 30 );
+
+        return $this->_field_string_prefix . $field_id . " - option '" . $sanitized_string . "'";
     }
 
     /**
      * Adjusts HTML output for radio field.
      */
-    public function t_radio( $html, $option, $field_id, $selected, $k ) {
-        $label = $this->_t_option_name( $option, $field_id );
+    public function translate_radio( $html, $option, $field_id, $selected, $k ) {
+        $label = $this->translate_option_name( $option, $field_id );
+
         return preg_replace( "/\>{$option->name}\<\/label\>/", ">{$label}</label>", $html, 1 );
     }
 
     /**
      * Adjusts HTML output for checkbox field.
      */
-    public function t_checkbox( $html, $option, $field_id, $selected, $k ) {
-        return $this->t_radio( $html, $option, $field_id, $selected, $k );
+    public function translate_checkbox( $html, $option, $field_id, $selected, $k ) {
+        return $this->translate_radio( $html, $option, $field_id, $selected, $k );
     }
 
     /**
      * Adjusts HTML output for select field.
      */
-    public function t_select_option( $html, $option, $field_id, $selected, $k ) {
-        $label = $this->_t_option_name( $option, $field_id );
+    public function translate_select_option( $html, $option, $field_id, $selected, $k ) {
+        $label = $this->translate_option_name( $option, $field_id );
         return preg_replace( '/"\>(.*)\<\/option\>/', "\">{$label}</option>", $html );
     }
 
     /**
      * Adjusts HTML output for multiselect field.
      */
-    public function t_multiselect_option( $html, $option, $field_id, $selected, $k ) {
-        return $this->t_select_option( $html, $option, $field_id, $selected, $k );
+    public function translate_multiselect_option( $html, $option, $field_id, $selected, $k ) {
+        return $this->translate_select_option( $html, $option, $field_id, $selected, $k );
     }
 
     /**
      * Filters field values on profile view template.
      */
-    public function t_value_profile_view( $value, $field_type ) {
+    public function translate_value_profile_view( $value, $field_type ) {
         global $field;
 
         // Only for fields with options
@@ -285,15 +338,13 @@ class BPML_XProfile implements \IWPML_Backend_Action, \IWPML_Frontend_Action {
                     $_value = false;
                     foreach ( $options as $option ) {
                         if ( isset($option->name) && $option->name == $field->data->value ) {
-                            $_value = apply_filters( 'wpml_translate_single_string', $option->name, $this->_context,
-                                    $this->sanitize_option_basename( $option, $field->id ) . ' name' );
+                            $_value = $this->translate_option_name( $option, $field->id );
                         }
                     }
                     if ( $_value ) {
                         // Expected format is search link
-                        $value = str_replace( ">{$field->data->value}</a>",
-                                ">{$_value}</a>", $value, $count );
-                        if (!$count) {
+                        $value = str_replace( ">{$field->data->value}</a>", ">{$_value}</a>", $value, $count );
+                        if ( ! $count ) {
                             $value = $_value;
                         }
                     }
@@ -302,13 +353,11 @@ class BPML_XProfile implements \IWPML_Backend_Action, \IWPML_Frontend_Action {
                 case 'multiselectbox':
                 case 'checkbox':
                     foreach ( $options as $option ) {
-                        $_value = apply_filters( 'wpml_translate_single_string', $option->name, $this->_context,
-                                $this->sanitize_option_basename( $option, $field->id ) . ' name' );
+                        $_value = $this->translate_option_name( $option, $field->id );
                         // Expected format is search link
-                        $value = str_replace( ">{$option->name}</a>",
-                                ">{$_value}</a>", $value, $count );
+                        $value = str_replace( ">{$option->name}</a>", ">{$_value}</a>", $value, $count );
                         // CSV list
-                        if ( !$count && strpos( $value, $option->name ) !== false ) {
+                        if ( ! $count && strpos( $value, $option->name ) !== false ) {
                             $_ex_values = explode( ',', $value );
                             if ( !empty( $_ex_values ) ) {
                                 foreach ( $_ex_values as &$v ) {
@@ -326,10 +375,16 @@ class BPML_XProfile implements \IWPML_Backend_Action, \IWPML_Frontend_Action {
                     break;
             }
         }
+
         return $value;
     }
 
-    public function t_group_name( $group_name ) {
+    /**
+     * @param string $group_name
+     *
+     * @return string
+     */
+    public function translate_group_name( $group_name ) {
         $cache_key = 'bpml_xprofile_group_id_by_name_' . md5( $group_name );
         $group_id = wp_cache_get( $cache_key );
         if ( false === $group_id ) {
@@ -339,8 +394,7 @@ class BPML_XProfile implements \IWPML_Backend_Action, \IWPML_Frontend_Action {
             wp_cache_set( $cache_key, $group_id );
         }
         
-        return $group_id ? apply_filters( 'wpml_translate_single_string', $group_name, $this->_context,
-                "{$this->_group_string_prefix}{$group_id} name" ) : $group_name;
+        return $group_id ? $this->translate( $group_name, $this->_group_string_prefix . $group_id . ' name' ) : $group_name;
     }
 
     /**
@@ -351,26 +405,16 @@ class BPML_XProfile implements \IWPML_Backend_Action, \IWPML_Frontend_Action {
      *
      * @return array
      */
-    public function t_data( $data, $args ) {
+    public function translate_data( $data, $args ) {
         $field_id = xprofile_get_field_id_from_name( $args['field'] );
         if ( $field_id ) {
             $field_type = bp_xprofile_get_field_type( $field_id );
             if ( $field_type && in_array( $field_type->field_obj->type, self::FIELD_TYPES_WITH_OPTIONS ) ) {
-                $data = stripslashes( apply_filters(
-                    'wpml_translate_single_string',
-                    $data,
-                    $this->_context,
-                    $this->sanitize_option_basename( (object) [ 'name' => $data ], $field_id ) . ' name'
-                ) );
+                $data = $this->translate_option_name( (object) [ 'name' => $data ], $field_id );
             }
         }
 
         return $data;
-    }
-
-    public function sanitize_option_basename( $option, $field_id ) {
-        $sanitized_string = bpml_sanitize_string_name( $option->name, 30 );
-        return "{$this->_field_string_prefix}{$field_id} - option '{$sanitized_string}'";
     }
 
     protected function verify_nonce() {
